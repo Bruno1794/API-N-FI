0e60def35c040d97116806ddb213bf3ea1eddd5a
Bruno <brunocostasilva62@gmail.com>
2025-06-13 17:51:28 -0300

    correção do app.js

--- app.js
+++ app.js
@@ -1,32 +1,27 @@
 const express = require('express');
 const cors = require('cors');
+const compression = require('compression'); // importa depois do express
 const app = express();
 
-//e um middlewer  necessario para receber os dados em JSON
+app.use(compression()); // aplica antes de tudo
 app.use(express.json());
 
-//middlewer de requisição para evitar problema com cors
-app.use((req, res, next) => {
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    res.header("Acecess-Control-Allow-Headers", "content-type, Authorization");
-    app.use(cors());
-    next();
-
-});
-
-//controllers
+// Controllers
 const usuarios = require('./controllers/usuario');
 const login = require('./controllers/login');
 const category = require('./controllers/category');
 const bancos = require('./controllers/bank');
 
-//rostas para acessa controllers
+// Rotas organizadas
 app.use('/', usuarios);
 app.use('/login', login);
 app.use('/', category);
 app.use('/', bancos);
 
-//porta do servidor
-app.listen(8000, () => console.log("Servidor rodando port 8080"));
+app.listen(8000, () => console.log("Servidor rodando na porta 8000"));
